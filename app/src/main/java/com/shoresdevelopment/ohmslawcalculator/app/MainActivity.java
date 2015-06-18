3b39408b71b88e5895f84235f51b4dbdebeb68e4
jsho32 <justin.shores32@gmail.com>
2015-06-18 14:40:02 -0700

    Remove billing, convert donate to share.

--- app/src/main/java/com/shoresdevelopment/ohmslawcalculator/app/MainActivity.java
+++ app/src/main/java/com/shoresdevelopment/ohmslawcalculator/app/MainActivity.java
@@ -2,19 +2,14 @@ package com.shoresdevelopment.ohmslawcalculator.app;
 
 import android.app.Activity;
 import android.app.Dialog;
-import android.app.PendingIntent;
 import android.content.*;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.os.RemoteException;
 import android.view.*;
 import android.widget.*;
-import com.android.vending.billing.IInAppBillingService;
+
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,9 +19,6 @@ public class MainActivity extends Activity {
     private EditText volts, amps, ohms, watts;
     private List<EditText> editTexts;
     private Dialog dialog;
-    private IInAppBillingService mService;
-    private ServiceConnection mServiceConn;
-    private LicenseKey licenseKey;
     private static final String TAG = "com.shodev.ohmslawcalc";
 
     @Override
@@ -38,27 +30,9 @@ public class MainActivity extends Activity {
         initializeEditTexts();
         initializeButtons();
 
-        licenseKey = new LicenseKey();
-
         AdView adView = (AdView) this.findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
-
-        mServiceConn = new ServiceConnection() {
-            @Override
-            public void onServiceDisconnected(ComponentName name) {
-                mService = null;
-            }
-
-            @Override
-            public void onServiceConnected(ComponentName name, IBinder service) {
-                mService = IInAppBillingService.Stub.asInterface(service);
-            }
-        };
-
-        Intent serviceIntent = new Intent("com.android.vending.billing.InAppBillingService.BIND");
-        serviceIntent.setPackage("com.android.vending");
-        bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
     }
 
     @Override
@@ -87,42 +61,6 @@ public class MainActivity extends Activity {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (mService != null) {
-            unbindService(mServiceConn);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 1001) {
-            int responseCode = data.getIntExtra("RESPONSE_CODE", 0);
-            String purchaseData = data.getStringExtra("INAPP_PURCHASE_DATA");
-            String dataSignature = data.getStringExtra("INAPP_DATA_SIGNATURE");
-
-            if (resultCode == Activity.RESULT_OK) {
-                try {
-                    JSONObject jo = new JSONObject(purchaseData);
-                    String sku = jo.getString("productId");
-                    String token = jo.getString("purchaseToken");
-                    dialog.setTitle(getResources().getText(R.string.thanks_title));
-                    TextView description = (TextView) dialog.findViewById(R.id.alert_description);
-                    description.setText(getResources().getText(R.string.thanks_description));
-                    Button close = (Button) dialog.findViewById(R.id.alert_close);
-                    close.setText(getResources().getText(R.string.thanks_close));
-                    dialog.show();
-                    findViewById(R.id.donate_container).setVisibility(View.GONE);
-                    try {
-                        mService.consumePurchase(3, MainActivity.this.getPackageName(), token);
-                    } catch (RemoteException e) {
-                        e.printStackTrace();
-                    }
-                }
-                catch (JSONException e) {
-                    Toast.makeText(this, "Failed to parse purchase data.", Toast.LENGTH_SHORT).show();
-                    e.printStackTrace();
-                }
-            }
-        }
     }
 
     /** Initializes alert dialog */
@@ -175,7 +113,7 @@ public class MainActivity extends Activity {
         Button reset = (Button) findViewById(R.id.reset);
         Button help = (Button) findViewById(R.id.help);
         Button developer = (Button) findViewById(R.id.developer);
-        Button donate = (Button) findViewById(R.id.donate);
+        Button share = (Button) findViewById(R.id.donate);
 
         calculate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -214,18 +152,16 @@ public class MainActivity extends Activity {
             }
         });
 
-        donate.setOnClickListener(new View.OnClickListener() {
+        share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.donate_container).setVisibility(View.VISIBLE);
-                Button close = (Button) findViewById(R.id.close_donation);
-                close.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        findViewById(R.id.donate_container).setVisibility(View.GONE);
-                    }
-                });
-                populateListView();
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_SUBJECT, getResources().getString(R.string.app_name));
+                String sAux = "Download it here: \n\n";
+                sAux = sAux + "https://play.google.com/store/apps/details?id=com.shoresdevelopment.ohmslawcalculator.app \n\n";
+                intent.putExtra(Intent.EXTRA_TEXT, sAux);
+                startActivity(Intent.createChooser(intent, "Choose Sharing Medium"));
             }
         });
     }
@@ -305,95 +241,5 @@ public class MainActivity extends Activity {
             volts.setText(String.valueOf(voltsValue));
             amps.setText(String.valueOf(ampsValue));
         }
-    }
-
-    /** Populates the donate listview. */
-    public void populateListView() {
-        ListView listView = (ListView) findViewById(R.id.donateList);
-
-        List<DonateListItem> listItems = getListItems();
-        DonateListItem listContents[] = new DonateListItem[listItems.size()];
-
-        listItems.toArray(listContents);
-        listView.setAdapter(getListAdapter(listContents));
-    }
-
-    /** Creates the list of store list items */
-    private List<DonateListItem> getListItems() {
-        List<DonateListItem> listItems = new ArrayList<>();
-        ArrayList<String> skuList = new ArrayList<>();
-        skuList.add(licenseKey.getSmallDonation());
-        skuList.add(licenseKey.getMediumDonation());
-        skuList.add(licenseKey.getLargeDonation());
-        //skuList.add("android.test.purchased");
-        Bundle querySkus = new Bundle();
-        querySkus.putStringArrayList("ITEM_ID_LIST", skuList);
-
-        try {
-            Bundle skuDetails = mService.getSkuDetails(3, MainActivity.this.getPackageName(), "inapp", querySkus);
-
-            int response = skuDetails.getInt("RESPONSE_CODE");
-            if (response == 0) {
-                ArrayList<String> responseList = skuDetails.getStringArrayList("DETAILS_LIST");
-
-                for (String thisResponse : responseList) {
-                    JSONObject object = new JSONObject(thisResponse);
-                    String sku = object.getString("productId");
-                    String price = object.getString("price");
-                    String title = object.getString("title").replace("(Ohms Law Calculator)", "");
-                    String description = object.getString("description");
-
-                    DonateListItem item = new DonateListItem(title, description, sku, price);
-                    listItems.add(item);
-
-                }
-            }
-        } catch (RemoteException | JSONException e) {
-            e.printStackTrace();
-        }
-
-        return listItems;
-
-    }
-
-    /** Returns adapter for store list view */
-    private ArrayAdapter<DonateListItem> getListAdapter(final DonateListItem item[]) {
-
-        return new ArrayAdapter<DonateListItem>(MainActivity.this, R.layout.donate_list_item, item) {
-
-            @Override
-            public View getView(final int position, View convertView, ViewGroup parent) {
-                if (convertView == null) {
-                    LayoutInflater inflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                    convertView = inflater.inflate(R.layout.donate_list_item, null);
-                }
-
-                final TextView description = (TextView) convertView.findViewById(R.id.description);
-                description.setText(item[position].getDescription());
-                final TextView title = (TextView) convertView.findViewById(R.id.title);
-                title.setText(item[position].getTitle());
-
-                Button purchase = (Button) convertView.findViewById(R.id.purchase);
-                purchase.setText(item[position].getPrice());
-                purchase.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        try {
-                            Bundle buyIntentBundle = mService.getBuyIntent(3, MainActivity.this.getPackageName(),
-                                    item[position].getITEM_SKU(), "inapp", "");
-
-                            PendingIntent pendingIntent = buyIntentBundle.getParcelable("BUY_INTENT");
-                            MainActivity.this.startIntentSenderForResult(pendingIntent.getIntentSender(),
-                                    1001, new Intent(), Integer.valueOf(0), Integer.valueOf(0),
-                                    Integer.valueOf(0));
-                        } catch (RemoteException | IntentSender.SendIntentException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-
-                return convertView;
-            }
-        };
     }
 }
